4d543e5632991a19df984c9649adf8c5b3a4683a
marcospalomomendez <marcos.palomomendez.25@campusfp.es>
2025-10-07 21:30:31 +0200

    Add files via upload
    Ejercicio_2

---
+++ Ejercicio_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+/*
+    Saludo personalizado
+    Pide al usuario su nombre y muestra un mensaje: “Hola, [nombre], bienvenido al programa”.
+ */
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Pedir al usuario su nombre
+        Console.Write("Dime tu nombre: ");
+        String nombre = Console.ReadLine();
+        // Mostrar el nombre del usuario
+        Console.WriteLine($"Hola, {nombre}, bienvenido al programa.");
+    }
+}
