6d584b7d1e2f43899f440447aa23b6ac5ddd971a
marcospalomomendez <marcos.palomomendez.25@campusfp.es>
2025-10-07 21:31:06 +0200

    Add files via upload
    Ejercicio_3

---
+++ Ejercicio_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+/*
+    Edad en meses
+    Pide la edad del usuario y calcula cuántos meses ha vivido.
+    Muestra el resultado en consola.
+ */
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Pedir al usuario su edad en años
+        Console.Write("Dime tu edad: ");
+        int edad = Convert.ToInt32(Console.ReadLine());
+        // Calcular y mostrar su edad en meses
+        int edadEnMeses = edad * 12;
+        Console.WriteLine($"Has vivido aproximadamente {edadEnMeses} meses.");
+    }
+}
