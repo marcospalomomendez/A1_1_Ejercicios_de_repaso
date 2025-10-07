5e94631870696567867ced996055727fa48946d2
marcospalomomendez <marcos.palomomendez.25@campusfp.es>
2025-10-07 21:32:21 +0200

    Add files via upload
    Ejercicio_5

---
+++ Ejercicio_5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+/*
+    Aprobado o suspendido
+    Pide una nota y muestra “Aprobado” si es ≥ 5 o “Suspendido” si es < 5.
+ */
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Dime tu nota: ");
+        double nota = Convert.ToDouble(Console.ReadLine());
+        // Calcular y mostrar si ha aprobado (nota >= 5) o suspendido (nota < 5)
+        if (nota >= 5)
+        {
+            Console.WriteLine($"Has aprobado con un: {nota}.");
+        }
+        else
+        {
+            Console.WriteLine($"Has suspendido con un: {nota}.");
+        }
+    }
+}
