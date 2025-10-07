62e7dcf4af6c75a58cd67d0e136d4c1803e9438f
marcospalomomendez <marcos.palomomendez.25@campusfp.es>
2025-10-07 21:31:30 +0200

    Add files via upload
    Ejercicio_4

---
+++ Ejercicio_4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+/*
+    Mayor de dos números
+    Pide dos números y muestra cuál es mayor.
+    Si son iguales, muestra un mensaje “Los números son iguales”.
+ */
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Pedir al usuario dos números
+        Console.Write("Ingrese el primer número: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Ingrese el segundo número: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        // Comparar los dos números y decir cual es mayor y en el caso que sean iguales dice      //que son iguales
+        if (num1 > num2)
+        {
+            Console.WriteLine($"El número mayor es: {num1}.");
+        }
+        else if (num2 > num1)
+        {
+            Console.WriteLine($"El número mayor es: {num2}.");
+
+        }
+        else
+        {
+            Console.WriteLine("Los números son iguales.");
+        }
+    }
+}
