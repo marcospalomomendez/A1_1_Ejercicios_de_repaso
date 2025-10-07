34fece6b5ecf14d4b64fbd879f1d8c27c4472fdf
marcospalomomendez <marcos.palomomendez.25@campusfp.es>
2025-10-07 21:32:44 +0200

    Add files via upload
    Ejercicio_6

---
+++ Ejercicio_6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+/*
+    Número par o impar
+    Pide un número y muestra si es par o impar usando el operador módulo %.
+ */
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Pedir un número
+        Console.Write("Ingrese un número: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        // Calcular y mostrar si es par o impar con el operado %
+        if (num1 % 2 == 0)
+        {
+            Console.WriteLine($"El número {num1} es par.");
+        }
+        else
+        {
+            Console.WriteLine($"El número {num1} es impar.");
+        }
+    }
+}
